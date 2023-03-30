fc5457f15793e4e704d7f62407eb4fd9d78959f3
PAULA EDUARDA DE LIMA <B51097@fgv.edu.br>
2023-03-30 15:34:08 -0300

    adicione solução 2

---
+++ Solução/ex_2.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/bash
+
+echo "Para pegar a primeira linha, {grep -am1 ""  contos/*.txt cronica/*.txt critica/*.txt miscelanea/*.txt poesia/*.txt romance/*.txt teatro/*.txt traducao/*.txt > destino} 
+, onde destino é um novo arquivo que armazena todas as primeiras linhas.
+
+Em seguida, {cut -f 2- -d ' ' destino > final}, onde final é um novo arquivo sem o local do texto, apenas o título.
+
+Por fim, {sort -b final > ordenado} para ordenar os títulos alfabeticamente."
+minha_resposta="7.	Cenas da vida amaz�nica, de Jos� Ver�ssimo, 1899
+A Carteira, 1884
+A Constituinte perante a hist�ria, pelo sr. HOMEM DE MELO. �
+A Est�tua de Jos� de Alencar,1906
+A Heran�a, 1878
+A Morte de Francisco Otaviano, 1889
+A M�goa do infeliz Cosme, 1875
+A M�o e a Luva,1874
+A Vi�va Sobral, 1884
+A chave, 1879
+A cr�tica teatral, A cr�tica teatral. Jos� de Alencar : M�e,
+A id�ia do Ezequiel Maia, 1883
+A inglesinha Barcelos, 1894
+A melhor das noivas, 1877
+A mulher p�lida, 1881
+A nova gera��o, 1879
+A paix�o de Jesus, 1904
+A pianista, 1866
+A reforma pelo jornal, 1859
+A semana, 1892
+A vida eterna, 1870
+A �ltima receita, 1875
+Alberto de Oliveira: Meridionais, 1884
+Almas Agradecidas, 1871.htm
+Americanas, 1875
+Antes a Rocha Tarp�ia, 1887
+Antes da Missa, 1878
+Antes que cases, 1875
+Ao Acaso, 1864.htm
+Aquarelas, 1859
+As Forcas Caudinas, 1956
+Ast�cias de marido, 1866
+Ayres e Vergueiro, 1871
+Badaladas, 1871
+Bagatela, 1859
+Balas de estalo,1883
+Bons dias,1888
+Brincar com fogo, 1875
+Cantiga velha, 1883
+Carlos Jansen: Contos seletos das mil e uma noites, 1882
+Carta ao Sr. Bispo do RJ, 1862
+Casa Velha,1885
+Casa Velha,1885
+Casa, n�o casa, 1875
+Casada e vi�va, 1864
+Castro Alves, 1868
+Cherchez la Femme, 1881
+Cinco mulheres, 1865
+Coment�rios da Semana, 1861
+Como se inventaram os almanaques, 1890
+Contos Fluminenses, 1870
+Convers�o de um avaro, 1878
+Cris�lidas, 1864
+Cronicas (O Futuro), 1862
+Cronicas do Dr. Semana, 1861
+Cr�tica teatral, 1906
+Curta Hist�ria, 1886
+D. M�nica, 1876
+Decad�ncia de dois grandes homens, 1873
+Desencantos, 1861
+Diana, 1866
+Discursos na Academia Brasileira de Letras, 1897
+Dom Casmurro, 1899
+Dona Jucunda, 1889
+D�vida extinta, 1878
+Eduardo Prado, 1901
+Elogio da Vaidade, 1878
+Encher tempo, 1876
+Entre 1892 e 1894, 1892
+Entre duas datas, 1884
+En�ias Galv�o: Miragens, 1885
+Esa� e Jac�,1904
+E�a De Queir�s: O Primo Bas�lio, 1878
+E�a de Queir�s, 1900
+Fagundes Varela - Cantos e fantasias, 1866
+Fagundes Varela, 1875
+Falenas, 1870
+Felicidade pelo casamento, 1866
+Fernando e Fernanda, 1866
+Filosofia de um Par de Botas, 1878
+Flor An�nima, 1897
+Flores e Frutos, de Bruno Seabra, 1862
+Fluminenses,1867
+Folha Rota, 1878
+Francisca, 1867
+Francisco de Castro: Harmonias errantes, 1878.
+Garrett, 1899
+Gazeta de Holanda, 1886
+Gon�alves Dias, 1906
+Habilidoso, 1885
+Helena,1876
+Henrique Chaves, 1893
+Henrique Lombaerts, 1897
+Henriqueta Renan, 1896
+ Historia de quinze dias, 1876
+Historia dos Trinta Dias, 1878
+Historias da Meia-Noite, 1873
+Hist�ria comum, 1883
+Hist�ria de uma fita azul, 1876
+Hist�ria de uma l�grima, 1867
+Hist�rias sem Data, 1884
+Hoje avental, amanh� luva, 1860
+Iai� Garcia,1878
+Identidade, 1887
+Id�ias sobre o Teatro, 1859
+J.M. de Macedo: O culto do dever, 1866
+Joaquim Nabuco: Pens�es d�tach�es et souvenirs, 1906
+Joaquim Serra, 1888
+Jogo do Bicho, 1904
+Jos� de Alencar , 1883
+Jos� de Alencar: Iracema, 186
+Jos� de Alencar: O Guarani, 1887
+Jo�o Fernandes, 1894
+Junqueira Freire - Inspira��es do claustro, 1866
+L. L. Fernandes Pinheiro J�nior: Tipos e quadros, 1886
+Letra Vencida, 1882
+Li��o de Bot�nica, 1906
+Longe dos Olhos, 1876
+L�cio de Mendon�a: N�voas matutinas, 1872
+Magalh�es de Azeredo: Horas sagradas; M�rio de Alencar: Versos, 1902
+Magalh�es de Azeredo: Procel�rias, 1898
+Mariana, 1871
+Memorial de Aires,1908
+Mem�rias P�stumas de Br�s Cubas, 1880
+Metaf�sica das Rosas, 1883
+Miloca, 1874
+Muitos Anos Depois, 1874
+M�dico � rem�dio, 1883
+Nem uma nem outra, 1873
+Notas semanais, 1878
+Not�cia da atual literatura brasileira, 1873
+N�o Consultes M�dico, 1899
+N�o � mel para boca de asno, 1868
+O Almada, 1910
+O Astr�logo, 1876
+O Bote de Rap�, 1878
+O Califa de Platina, 1878
+O Caminho de Damasco, 1871
+O Caso Barreto, 1892.htm
+O Caso da Vi�va, 1881
+O Caso do Romualdo, 1884
+O Escriv�o Coimbra, 1906
+O Jornal e o Livro, 1859
+O Machete, 1878
+O Melhor Rem�dio, 1884
+O Passado, o passado, 1876
+O Pa�s das quimeras, 1862
+ O Velho Senado, 1898
+O Visconde de Castilho, 1875
+O anjo Rafael, 1869
+O anjo das donzelas, 1864
+O caminho da porta - O protocolo, 1863
+O capit�o Mendon�a, 1870
+O carro n� 13, 1868
+O contrato, 1884
+O destinado, 1883
+O futuro dos argentinos, 1888
+O ideal do cr�tico, 1865
+O imortal, 1882
+O or�culo, 1866
+O pai, 1866
+O passado, o presente e o futuro da literatura, 1858
+O programa, 1882
+O que s�o as mo�as, 1866
+O rei dos caiporas, 1870
+O sainete, 1875
+O �ltimo dia de um poeta, 1867
+Ocidentais, 1901
+Oliveira Lima: Secret�rio D�El-Rei, 1904
+Onda, 1867
+Onze anos depois, 1875
+Orai por ele!, 1895
+Os Deuses de casaca, 1866
+Os Imortais, 1859
+Os �culos de Pedro Ant�o, 1874
+Pap�is Avulsos, 1882
+Pareceres de Machado de Assis, 1862
+Pedro Lu�s, 1884
+"
+echo $minha_resposta > resposta_ex_2
