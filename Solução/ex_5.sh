c639283e6edf341e3ba7068ac0b7edbfffe153b9
PAULA EDUARDA DE LIMA <paulaeduardalima09@gmail.com>
2023-04-01 17:51:47 -0300

    adicione resposta 5 e 6

--- Solução/ex_5.sh
+++ Solução/ex_5.sh
@@ -1,3 +1,7 @@
-#!usr/bin/bash
-cat obracompleta | sed 's/ /\n/g' | tr A-Z a-z > palavras
-tr -d '[:punct:]' <palavras | sort | uniq -c | sort -n > resposta_ex_5
+#!/usr/bin/bash
+echo "cat obracompleta | sed 's/ /\n/g' | tr A-Z a-z | tr -d '[:punct:]' | tr -d '[:digit:]'| iconv -f iso-8859-1 -t UTF-8 | sort -b | uniq ->
+"
+
+cat obracompleta | sed 's/ /\n/g' | tr A-Z a-z | tr -d '[:punct:]' | tr -d '[:digit:]'| iconv -f iso-8859-1 -t UTF-8 | sort -b | uniq -c | sort -n  > reposta_ex_5
+
+
